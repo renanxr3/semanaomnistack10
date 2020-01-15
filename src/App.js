256d0af4346ae44ff026d150737ce9227b0c4058
renan.santos <renan.a.santos.88@gmail.com>
2020-01-15 20:52:50 -0300

    Antes refatorar codigo App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,140 @@
-import React, { useState } from 'react';
-import Header from "./Header"
+import React, { useState, useEffect } from 'react'; 
+import api from "./services/api";
+
+import "./global.css"
+import "./App.css"
+import "./Sidebar.css"
+import "./Main.css"
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual nao interfere no restante da aplicacao
 // Propriedade: Informacoes que um componente PAI passa para o componente FILHO
-// Estado:  
+// Estado: Informacoes mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
-  const [counter, setCounter] = useState(0);
+const [devs, setDevs] = useState([]); 
 
-  function incrementarCounter() {
-    setCounter(counter+1);
+  const [github_username, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // console.log(position);
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      }, 
+      {
+        timeout: 30000, 
+      }
+    )
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    // console.log(response.data);
+
+    setGithubUsername("");
+    setTechs("");
+
+    setDevs([...devs, response.data]);
   }
 
   return (
-    <>
-      <h1> Contador: {counter} </h1>
-      <button onClick={incrementarCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div class="input-block">
+            <label htmlFor="username_github">Usuario do Github</label>
+            <input 
+              name="github_username" 
+              id="username_github" 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)} />
+          </div>
+          
+          <div class="input-block">
+            <label htmlFor="techs">Tecnlogias</label>
+            <input 
+              name="techs" 
+              id="techs" 
+              required 
+              value={techs}
+              onChange={e => setTechs(e.target.value)} />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+                type="number"
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude} 
+                onChange={e => setLatitude(e.target.value)} />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude}
+                onChange={e => setLatitude(e.target.value)} />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name}/>
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(", ")}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${github_username}`}>Acessar perfil no Github</a>
+            </li>
+          ))}          
+        </ul>
+      </main>
+    </div>
   );
 }
 
