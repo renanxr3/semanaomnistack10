7af9b0d1ee5feedc82ab5284bf6b4c293bdc9d2b
renan.santos <renan.a.santos.88@gmail.com>
2020-01-15 18:55:00 -0300

    Exemplo - Botao Incrementar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from "./Header"
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual nao interfere no restante da aplicacao
+// Propriedade: Informacoes que um componente PAI passa para o componente FILHO
+// Estado:  
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementarCounter() {
+    setCounter(counter+1);
+  }
+
   return (
-    <h1> Hello World! </h1>
+    <>
+      <h1> Contador: {counter} </h1>
+      <button onClick={incrementarCounter}>Incrementar</button>
+    </>
   );
 }
 
 export default App;
+
+// <>
+// <Header title="Dashboard 1"/>
+// <Header title="Dashboard"/>
+// <Header title="Dashboard 3"/>
+// </>
