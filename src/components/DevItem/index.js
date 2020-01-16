f18b26cd0451083fe2518d8448b629a7d8afa030
renan.santos <renan.a.santos.88@gmail.com>
2020-01-15 21:32:25 -0300

    App finalizado

--- src/components/DevItem/index.js
+++ src/components/DevItem/index.js
@@ -1,9 +1,21 @@
-import React from "React";
+import React from "react";
 
-function DevItem() {
+import "./styles.css";
+
+function DevItem({ dev }) {
     return (
-
+        <li className="dev-item">
+            <header>
+                <img src={dev.avatar_url} alt={dev.name}/>
+                <div className="user-info">
+                <strong>{dev.name}</strong>
+                <span>{dev.techs.join(", ")}</span>
+                </div>
+            </header>
+            <p>{dev.bio}</p>
+            <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+        </li>
     );
 }
 
-export default DevItem.
+export default DevItem;
