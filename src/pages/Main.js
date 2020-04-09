d61d5d3ca642a11ca1df7a024ac9f3ce508de4a1
Renan <renan.a.santos.88@gmail.com>
2020-04-09 16:58:00 -0300

    Changes for README

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,204 +1,212 @@
-import React, { useState, useEffect }  from 'react';
-import { StyleSheet, Image,  View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
-import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 import { MaterialIcons } from "@expo/vector-icons";
 
-import api from "../services/api"
-import { connect, disconnect, subscribeToNewDevs } from "../services/socket"
+import api from "../services/api";
+import { connect, disconnect, subscribeToNewDevs } from "../services/socket";
 
 function Main({ navigation }) {
-    const [devs, setDevs] = useState([]);
-    const [currentRegion, setCurrentRegion] = useState(null); 
-    const [techs, setTechs] = useState("");
+  const [devs, setDevs] = useState([]);
+  const [currentRegion, setCurrentRegion] = useState(null);
+  const [techs, setTechs] = useState("");
 
-    useEffect(() => {
-        async function loadInitialPosition() {
-            const { granted } = await requestPermissionsAsync();
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
 
-            if (granted) {
-                const { coords } = await getCurrentPositionAsync({
-                    enableHighAccuracy: true,
-                });
-
-                const { latitude, longitude } = coords;
-
-                setCurrentRegion({
-                    latitude,
-                    longitude,
-                    latitudeDelta: 0.03,
-                    longitudeDelta: 0.03,
-                })
-            }
-        }
-
-        loadInitialPosition();
-    }, []);
-
-    useEffect(() => {
-        subscribeToNewDevs(dev => setDevs([...devs, dev]));
-    }, [devs]);
-
-    function setupWebSocket() {
-        disconnect();
-
-        const { latitude, longitude } = currentRegion;
-        
-        connect(
-            latitude,
-            longitude,
-            techs
-        );
-    } 
-
-    async function loadDevs() {
-        const { latitude, longitude } = currentRegion;
-        
-        const response = await api.get("/search", {
-            params: {
-                latitude,
-                longitude,
-                techs,
-            }
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
         });
 
-        // console.log(response.data);
+        const { latitude, longitude } = coords;
 
-        setDevs(response.data.devs);
-        setupWebSocket();
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.03,
+        });
+      }
     }
 
-    function handleRegionChanged(region) {
-        // console.log(region);
-        setCurrentRegion(region);
-    }
+    loadInitialPosition();
+  }, []);
 
-    if (!currentRegion) {
-        return null;
-    }
+  useEffect(() => {
+    subscribeToNewDevs((dev) => setDevs([...devs, dev]));
+  }, [devs]);
 
-    return (
-        <>
-            <MapView 
-                onRegionChangeComplete={handleRegionChanged}
-                initialRegion={currentRegion}
-                style={{ flex: 1 }} 
+  function setupWebSocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
+
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    console.log(currentRegion);
+
+    const response = await api.get("/search", {
+      params: {
+        latitude,
+        longitude,
+        techs,
+      },
+    });
+
+    console.log(response.data);
+
+    setDevs(response.data.devs);
+    setupWebSocket();
+  }
+
+  function handleRegionChanged(region) {
+    // console.log(region);
+    setCurrentRegion(region);
+  }
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <>
+      <MapView
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+        style={{ flex: 1 }}
+      >
+        {devs.map((dev) => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              latitude: dev.location.coordinates[1],
+              longitude: dev.location.coordinates[0],
+            }}
+          >
+            <Image style={styles.avatar} source={{ uri: dev.avatar_url }} />
+
+            <Callout
+              onPress={() => {
+                navigation.navigate("Profile", {
+                  github_username: dev.github_username,
+                });
+              }}
             >
-                {devs.map(dev => (
-                    <Marker 
-                        key={dev._id}
-                        coordinate={{ 
-                            latitude: dev.location.coordinates[1], 
-                            longitude: dev.location.coordinates[0], 
-                        }} 
-                    >
-                        <Image style={styles.avatar} source={{ uri: dev.avatar_url }} />
+              <View style={styles.callout}>
+                <Text style={styles.devName}> {dev.name} </Text>
+                <Text style={styles.devBio}> {dev.bio} </Text>
+                <Text style={styles.devTechs}> {dev.techs.join(", ")} </Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+          value={techs}
+          onChangeText={(text) => setTechs(text)} // ou onChangeText={setTechs(text)}
+        />
 
-                        <Callout onPress={() => {
-                            navigation.navigate("Profile", { github_username: dev.github_username });
-                        }}>
-                            <View style={styles.callout}>
-                                <Text style={styles.devName}> {dev.name} </Text>
-                                <Text style={styles.devBio}> {dev.bio} </Text>
-                                <Text style={styles.devTechs}> {dev.techs.join(", ")} </Text>
-                            </View>
-                        </Callout>
-                    </Marker>
-                ))}
-            </MapView>
-            <View style={styles.searchForm}>
-                <TextInput
-                    style={styles.searchInput} 
-                    placeholder="Buscar devs por techs..."    
-                    placeholderTextColor="#999"
-                    autoCapitalize="words"
-                    autoCorrect={false}        
-                    value={techs}
-                    onChangeText={text => setTechs(text)}   // ou onChangeText={setTechs(text)}
-                />
-
-                <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
-                    <MaterialIcons name="my-location" size={20} color="#FFF" />
-                </TouchableOpacity>
-            </View>
-        </>
-    );
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    map: {
-        flex: 1
+  map: {
+    flex: 1,
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 5,
+    borderWidth: 4,
+    borderColor: "#FFF",
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: "#666",
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
+  },
+
+  loadButton: {},
+
+  searchForm: {
+    position: "absolute",
+    // bottom: 20, // Alinhar embaixo da tela
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row",
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFF",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
     },
+    elevation: 2,
+  },
 
-    avatar: {
-        width: 54,
-        height: 54, 
-        borderRadius: 5,
-        borderWidth: 4,
-        borderColor: "#FFF",
-    }, 
-
-    callout: {
-        width: 260,
-    }, 
-
-    devName: {
-        fontWeight: "bold",
-        fontSize: 16,
-    },
-
-    devBio: {
-        color: "#666",
-        marginTop: 5,
-    }, 
-
-    devTechs: {
-        marginTop: 5,
-    }, 
-
-    loadButton: {
-
-    }, 
-
-    searchForm: {
-        position: "absolute",
-        // bottom: 20, // Alinhar embaixo da tela
-        top: 20,
-        left: 20,
-        right: 20,
-        zIndex: 5,
-        flexDirection: "row",
-    }, 
-
-    searchInput: {
-        flex: 1,
-        height: 50,
-        backgroundColor: "#FFF",
-        color: "#333",
-        borderRadius: 25,
-        paddingHorizontal: 20,
-        fontSize: 16,
-        shadowColor: "#000",
-        shadowOpacity: 0.2,
-        shadowOffset: {
-            width: 4,
-            height: 4,
-        },
-        elevation: 2,
-    }, 
-
-    loadButton: {
-        width: 50,
-        height: 50,
-        backgroundColor: "#8E4Dff",
-        borderRadius: 25,
-        justifyContent: "center",
-        alignItems: "center",
-        marginLeft: 15,
-
-    }, 
-
-
-})
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8E4Dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15,
+  },
+});
 
 export default Main;
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.0.11:3001",
-
+  baseURL: "http://192.168.0.37:3001",
 });
 
 export default api;
